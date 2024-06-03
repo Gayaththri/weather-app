9b68eb435c54c088496c1b66af19c40d99521fcb
Gayaththri <sriigaya3@gmail.com>
2024-06-03 20:58:13 +0530

    Add getWeatherData function to fetch weather data from OpenWeather API

---
+++ src/services/weatherService.js
@@ -0,0 +1,9 @@
+const API_KEY = "e021a0472b7d9be2e09d022e50b9ca3f";
+const BASE_URL = "https://api.openweathermap.org/data/2.5";
+
+const getWeatherData = (infoType, searchParams) => {
+  const url = new URL(BASE_URL + "/" + infoType);
+  url.search = new URLSearchParams({ ...searchParams, appid: API_KEY });
+
+  return fetch(url).then((res) => res.json());
+};
