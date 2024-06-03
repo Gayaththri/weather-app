cb9c7116b0156fc88a34f35ff9c82ca6bcdedaf4
Gayaththri <sriigaya3@gmail.com>
2024-06-03 11:16:25 +0530

    Add TimeAndLocation component with date, time, and location display and added poppins as font style

---
+++ src/components/TimeAndLocation.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function TimeAndLocation() {
+  return (
+    <div>
+      <div className="flex items-center justify-center my-6">
+        <p className="text-white text-xl font-extralight">
+          Monday, 6 June 2024 | Local time: 11:05 AM
+        </p>
+      </div>
+      <div className="flex items-center justify-center my-3">
+        <p className="text-white text-3xl">Colombo, SL</p>
+      </div>
+    </div>
+  );
+}
+
+export default TimeAndLocation;
