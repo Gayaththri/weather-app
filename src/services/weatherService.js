c73a235bf08280a857b1894102072724d6983aa9
Gayaththri <sriigaya3@gmail.com>
2024-06-06 19:08:48 +0530

    Add weather data formatting functions and integrate with weather fetching service

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -1,3 +1,5 @@
+import { DateTime } from "luxon";
+
 const API_KEY = "e021a0472b7d9be2e09d022e50b9ca3f";
 const BASE_URL = "https://api.openweathermap.org/data/2.5";
 
@@ -40,13 +42,56 @@ const formatCurrentWeather = (data) => {
   };
 };
 
+const formatForecastWeather = (data) => {
+  let { timezone, daily, hourly } = data;
+  if (daily && hourly) {
+    daily = daily.slice(1, 6).map((d) => {
+      return {
+        title: formatToLocalTime(d.dt, timezone, "ccc"),
+        temp: d.temp.day,
+        icon: d.weather[0].icon,
+      };
+    });
+
+    hourly = hourly.slice(1, 6).map((d) => {
+      return {
+        title: formatToLocalTime(d.dt, timezone, "hh:mm a"),
+        temp: d.temp.day,
+        icon: d.weather[0].icon,
+      };
+    });
+  } else {
+    // Handle the case where daily or hourly is undefined
+    // For example, you might want to set them to empty arrays
+    daily = [];
+    hourly = [];
+  }
+
+  return { timezone, daily, hourly };
+};
+
 const getFormattedWeatherData = async (searchParams) => {
   const formattedCurrentWeather = await getWeatherData(
     "weather",
     searchParams
   ).then(formatCurrentWeather);
 
-  return formattedCurrentWeather;
+  const { lat, lon } = formattedCurrentWeather;
+
+  const formattedForecastWeather = await getWeatherData("onecall", {
+    lat,
+    lon,
+    exclude: "current, minutely, alerts",
+    units: searchParams.units,
+  }).then(formatForecastWeather);
+
+  return { ...formattedCurrentWeather, ...formattedForecastWeather };
 };
+
+const formatToLocalTime = (
+  secs,
+  zone,
+  format = "cccc, dd LLL yyyy' | Local time: 'hh:mm a"
+) => DateTime.fromSeconds(secs).setZone(zone).toFormat(format);
 
 export default getFormattedWeatherData;
